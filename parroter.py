09d3c21e3c1948fe353f9db88230c3b50031eb84
Erin Morelli <erin@erinmorelli.com>
2018-03-21 02:47:51 +0000

    Version 0.5
    * Add notification support for automated adding (#2)
    * Add support for Party Guests (#5)
    * Deal with multi-page emoji pages (#6)

--- parroter.py
+++ parroter.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2017 Erin Morelli.
+Copyright (c) 2017-2018 Erin Morelli.
 
 Title       : EM Slack Party Parroter
 Author      : Erin Morelli
 Email       : erin@erinmorelli.com
 License     : MIT
-Version     : 0.4
+Version     : 0.5
 """
 
 # Future
@@ -35,11 +35,11 @@ except ImportError:
 
 # Script credits
 __title__ = 'EM Slack Party Parroter'
-__copyright__ = 'Copyright (c) 2017, Erin Morelli'
+__copyright__ = 'Copyright (c) 2017-2018, Erin Morelli'
 __author__ = 'Erin Morelli'
 __email__ = 'erin@erinmorelli.com'
 __license__ = 'MIT'
-__version__ = '0.4'
+__version__ = '0.5'
 
 # Disable SSL warnings
 urllib3.disable_warnings()
@@ -50,24 +50,28 @@ try:
 except NameError:
     raw_input = input
 
-# Build the Party Parrot GitHub raw file URL
-PARROT_ROOT = os.path.join(
-    'https://raw.githubusercontent.com',  # GitHub raw file root
-    'jmhobbs',                            # Username
-    'cultofthepartyparrot.com',           # Repository
-    'master',                             # Branch
-    '{0}'
-)
-
 
 class EmSlackPartyParroter(object):
     """Class to interact with the Slack emoji customizer."""
 
+    API_ROOT = 'https://slack.com/api/{0}'
+    PARROT_ROOT = os.path.join(
+        'https://raw.githubusercontent.com',  # GitHub raw file root
+        'jmhobbs',                            # Username
+        'cultofthepartyparrot.com',           # Repository
+        'master',                             # Branch
+        '{0}'
+    )
+
     def __init__(self):
         """Initialize class and connect to Slack."""
         self._parrot = {
-            'json': PARROT_ROOT.format('parrots.json'),
-            'img': PARROT_ROOT.format('parrots')
+            'json': self.__class__.PARROT_ROOT.format('parrots.json'),
+            'img': self.__class__.PARROT_ROOT.format('parrots')
+        }
+        self._guest = {
+            'json': self.__class__.PARROT_ROOT.format('guests.json'),
+            'img': self.__class__.PARROT_ROOT.format('guests')
         }
 
         # Build argparser
@@ -98,6 +102,9 @@ class EmSlackPartyParroter(object):
         self.session = requests.Session()
         self._load_cookie_jar(refresh=self.args.refresh)
 
+        # Get API token for session
+        self.api_token = self._get_api_token()
+
     def _build_parser(self):
         """Build the CLI argument parser."""
         self._parser = argparse.ArgumentParser(
@@ -115,7 +122,7 @@ class EmSlackPartyParroter(object):
 
         # Add credential arguments
         auth_group.add_argument(
-            '--team', '-t',
+            '-t', '--team',
             default=os.getenv('SLACK_TEAM'),
             help='\n'.join(
                 [
@@ -126,7 +133,7 @@ class EmSlackPartyParroter(object):
             type=str.lower
         )
         auth_group.add_argument(
-            '--email', '-e',
+            '-e', '--email',
             default=os.getenv('SLACK_EMAIL'),
             help='\n'.join(
                 [
@@ -136,7 +143,7 @@ class EmSlackPartyParroter(object):
             )
         )
         auth_group.add_argument(
-            '--password', '-p',
+            '-p', '--password',
             default=os.getenv('SLACK_PASSWORD'),
             help='\n'.join(
                 [
@@ -145,22 +152,39 @@ class EmSlackPartyParroter(object):
                 ]
             )
         )
+        auth_group.add_argument(
+            '-c', '--channel',
+            default=os.getenv('SLACK_CHANNEL'),
+            help='\n'.join(
+                [
+                    'Slack channel name to send new parrot messages to.',
+                    'Defaults to the $SLACK_CHANNEL environment variable.',
+                    'Can be a public channel, private channel, or username.',
+                    'e.g. "#public-channel", "private-channel", "@username"'
+                ]
+            )
+        )
 
         # Add optional arguments
         self._parser.add_argument(
-            '--list_existing', '-l',
+            '-g', '--include_guests',
             action='store_true',
-            help='Displays a list of your Slack team\'s existing parrots'
+            help='Add Party Guests to your team in addition to standard parrots.'
         )
         self._parser.add_argument(
-            '--list_available', '-a',
+            '-l', '--list_existing',
             action='store_true',
-            help='Displays a list of all available parrots'
+            help='Displays a list of your Slack team\'s existing parrots.'
         )
         self._parser.add_argument(
-            '--list_new', '-n',
+            '-a', '--list_available',
             action='store_true',
-            help='Displays a list of new parrots for your Slack team'
+            help='Displays a list of all available parrots.'
+        )
+        self._parser.add_argument(
+            '-n', '--list_new',
+            action='store_true',
+            help='Displays a list of new parrots for your Slack team.'
         )
         self._parser.add_argument(
             '-r', '--refresh',
@@ -171,7 +195,7 @@ class EmSlackPartyParroter(object):
         self._parser.add_argument(
             '-q', '--quiet',
             default=False,
-            help='Don\'t prompt for approval to add parrots',
+            help='Don\'t prompt for approval to add parrots.',
             action='store_true'
         )
 
@@ -400,6 +424,8 @@ class EmSlackPartyParroter(object):
         )
         sys.exit(1)
 
+        return None
+
     def _get_cookies(self):
         """Login to Slack to retrieve user cookies.
 
@@ -451,6 +477,33 @@ class EmSlackPartyParroter(object):
         # Return logged in session
         return validated.cookies
 
+    def _get_api_token(self):
+        """Retrieve the user's session API key."""
+        emoji_page = self.session.get(self.emoji_url)
+        emoji_page.raise_for_status()
+
+        # Set regex to find token
+        token_regex = r'api_token: \"(xoxs-.+)\",?\n'
+
+        # Find the session token on page
+        token = re.findall(token_regex, emoji_page.text)
+
+        # Check for result
+        if not token:
+            print(
+                ' '.join(
+                    [
+                        'ERROR: There was a problem logging in to Slack.',
+                        'Check your team, email, and password and try again.'
+                    ]
+                ),
+                file=sys.stderr
+            )
+            sys.exit(1)
+
+        # Otherwise return token
+        return token[0]
+
     def yes_or_no(self, question):
         """Prompts the user for a yes or no answer to a question.
 
@@ -470,17 +523,40 @@ class EmSlackPartyParroter(object):
 
     def get_current_emoji_list(self):
         """Retrieve list of current Slack team emoji."""
-        emoji_page = self.session.get(self.emoji_url)
-        emoji_page.raise_for_status()
+        emoji_res = self.session.get(
+            self.__class__.API_ROOT.format('emoji.list'),
+            params={
+                'token': self.api_token
+            }
+        )
+        emoji_res.raise_for_status()
 
-        # Set emoji name parsing regex
-        emoji_regex = r'data-emoji-name=\"(.*?)\"'
+        # Get JSON response
+        emoji_list = emoji_res.json()
 
-        # Find all emoji names on page
-        emoji = re.findall(emoji_regex, emoji_page.text)
+        # Check for response errors
+        if not emoji_list['ok'] or 'emoji' not in emoji_list.keys():
+            print(
+                'ERROR: Unable to load Slack emoji',
+                file=sys.stderr
+            )
+            sys.exit(1)
 
-        # Return list of emoji
-        return emoji
+        # Return list of current emoji
+        return emoji_list['emoji'].keys()
+
+    def get_guests_list(self):
+        """Retrieve up-to-date guests list from GitHub.
+
+        Returns:
+            list: Parsed JSON list of current guests
+
+        """
+        guests = requests.get(self._guest['json'])
+        guests.raise_for_status()
+
+        # Return parsed JSON
+        return guests.json()
 
     def get_parrot_list(self):
         """Retrieve up-to-date parrot list from GitHub.
@@ -495,45 +571,69 @@ class EmSlackPartyParroter(object):
         # Return parsed JSON
         return parrots.json()
 
-    def get_parrots_to_add(self, parrots, current_emoji):
-        """Determine which parrots are not already added to the team.
+    def get_emoji_list(self, current_emoji, emojis, is_guest=False):
+        """Get a formatted list of emoji.
 
         Args:
-            parrots (list): List of available parrot emojis
-            current_emoji (list): List of current team emojis
+            current_emoji (list): All current Slack emojis for the team
+            emojis (list): Available emoji from CultOfThePartyParrot
+            is_guest (bool): Whether or not these are Party Guest emojis
 
         Returns:
-            list: Parrot emojis to be added to the team
+            list: Formatted list of dicts of emoji data
 
         """
-        parrots_to_add = []
+        emoji_list = []
 
         # Loop over all parrots
-        for parrot in parrots:
+        for emoji in emojis:
+            # Set whether this is a part guest or not
+            emoji['is_guest'] = is_guest
+
             # Check if we can use an HD image
-            if 'hd' in parrot.keys():
-                parrot['use_hd'] = True
-                parrot['slug'] = re.split(r'\/|\.', parrot['hd'])[1]
+            if 'hd' in emoji.keys():
+                emoji['use_hd'] = True
+                emoji['slug'] = re.split(r'\/|\.', emoji['hd'])[1]
 
             # Otherwise use the standard GIF image
-            elif 'gif' in parrot.keys():
-                parrot['use_hd'] = False
-                parrot['slug'] = re.split(r'\.', parrot['gif'])[0]
+            elif 'gif' in emoji.keys():
+                emoji['use_hd'] = False
+                emoji['slug'] = re.split(r'\.', emoji['gif'])[0]
 
             # If we're only listing, do that
             if self.args.list_available:
                 print(
-                    ':{parrot}:'.format(parrot=parrot['slug']),
+                    ':{emoji}:'.format(emoji=emoji['slug']),
                     file=sys.stdout
                 )
                 continue
 
             # Add the parrot if it doesn't already exist
-            if parrot['slug'] not in current_emoji:
-                parrots_to_add.append(parrot)
+            if emoji['slug'] not in current_emoji:
+                emoji_list.append(emoji)
 
-        # Return list of parrots to be added
-        return parrots_to_add
+        return emoji_list
+
+    def get_parrots_to_add(self, current_emoji, parrots, guests=None):
+        """Determine which parrots are not already added to the team.
+
+        Args:
+            current_emoji (list): List of current team emojis
+            parrots (list): List of available parrot emojis
+            guests (list): List of available guest emojis
+
+        Returns:
+            list: Parrot emojis to be added to the team
+
+        """
+        parrots_to_add = self.get_emoji_list(current_emoji, parrots)
+        guests_to_add = []
+
+        if guests is not None:
+            guests_to_add = self.get_emoji_list(current_emoji, guests, True)
+
+        # Return list of parrots and guests to be added
+        return parrots_to_add + guests_to_add
 
     def add_parrot(self, parrot):
         """Upload a new parrot emoji to a Slack team.
@@ -548,7 +648,10 @@ class EmSlackPartyParroter(object):
         parrot_file = parrot['hd'] if parrot['use_hd'] else parrot['gif']
 
         # Get parrot image file path
-        parrot_url = os.path.join(self._parrot['img'], parrot_file)
+        if parrot['is_guest']:
+            parrot_url = os.path.join(self._guest['img'], parrot_file)
+        else:
+            parrot_url = os.path.join(self._parrot['img'], parrot_file)
 
         # Get parrot
         parrot_img = requests.get(parrot_url, stream=True)
@@ -586,82 +689,19 @@ class EmSlackPartyParroter(object):
             # Return error text
             return error.text
 
-    def parrot(self):
-        """Get and upload new parrots to Slack team.
+        return None
 
-        Returns:
-            list: List of added parrots
+    def add_parrots(self, parrots):
+        """Add new parrots.
+
+        Args:
+            parrots (list): List of dicts of parrot emoji data
 
         """
-        parrots = self.get_parrot_list()
-
-        # Get existing emoji list
-        current_emoji = self.get_current_emoji_list()
-
-        # If we're listing existing, do that and exit
-        if self.args.list_existing:
-            print('Existing Parrots:', file=sys.stdout)
-
-            # Only show parrot emojis
-            for emoji in current_emoji:
-                if re.search(r'parrot', emoji, re.I):
-                    print(
-                        ':{parrot}:'.format(parrot=emoji),
-                        file=sys.stdout
-                    )
-            sys.exit()
-
-        # Initialize added parrots tracker
         added = []
-        if self.args.list_available:
-            print('Available Parrots:', file=sys.stdout)
-        else:
-            print('Starting Parroting...', file=sys.stdout)
-
-        # Get parrots to add
-        parrots_to_add = self.get_parrots_to_add(parrots, current_emoji)
-
-        # Exit if we're just listing
-        if self.args.list_available:
-            sys.exit()
-
-        # Bail if there are no parrots to add
-        if not parrots_to_add:
-            print('No new parrots to add!', file=sys.stdout)
-            sys.exit()
-
-        # Report number of new parrots found
-        print(
-            'Found {count} new parrot{s} to add!'.format(
-                count=len(parrots_to_add),
-                s='' if len(parrots_to_add) == 1 else 's'
-            ),
-            file=sys.stdout
-        )
-
-        # List the parrots
-        for parrot in parrots_to_add:
-            print(
-                ':{parrot}:'.format(parrot=parrot['slug']),
-                file=sys.stdout
-            )
-
-        # Exit if we're just listing
-        if self.args.list_new:
-            sys.exit()
-
-        # Prompt user to continue
-        if self.args.quiet:
-            approved = True
-        else:
-            approved = self.yes_or_no('Add them to your Slack team?')
-
-        # Check for approval
-        if not approved:
-            sys.exit()
 
         # Loop over parrots to add
-        for parrot in parrots_to_add:
+        for parrot in parrots:
             # Add the parrot
             error = self.add_parrot(parrot)
 
@@ -688,6 +728,135 @@ class EmSlackPartyParroter(object):
         # Return list of added parrots
         return added
 
+    def parrot(self):
+        """Get and upload new parrots to Slack team.
+
+        Returns:
+            list: List of added parrots
+
+        """
+        parrots = self.get_parrot_list()
+        guests = []
+
+        # Get guests
+        if self.args.include_guests:
+            guests = self.get_guests_list()
+
+        # Get existing emoji list
+        current_emoji = self.get_current_emoji_list()
+
+        # If we're listing existing, do that and exit
+        if self.args.list_existing:
+            print('Existing Parrots:', file=sys.stdout)
+
+            # Only show parrot emojis
+            for emoji in current_emoji:
+                print(
+                    ':{parrot}:'.format(parrot=emoji),
+                    file=sys.stdout
+                )
+            sys.exit()
+
+        # Initialize CLI output
+        if self.args.list_available:
+            print('Available Parrots:', file=sys.stdout)
+        else:
+            print('Starting Parroting...', file=sys.stdout)
+
+        # Get parrots to add
+        to_add = self.get_parrots_to_add(current_emoji, parrots, guests)
+
+        # Exit if we're just listing
+        if self.args.list_available:
+            sys.exit()
+
+        # Bail if there are no parrots to add
+        if not to_add:
+            print('No new parrots to add!', file=sys.stdout)
+            sys.exit()
+
+        # Report number of new parrots found
+        print(
+            'Found {count} new parrot{s} to add!'.format(
+                count=len(to_add),
+                s='' if len(to_add) == 1 else 's'
+            ),
+            file=sys.stdout
+        )
+
+        # List the parrots
+        for parrot in to_add:
+            print(
+                ':{parrot}:'.format(parrot=parrot['slug']),
+                file=sys.stdout
+            )
+
+        # Exit if we're just listing
+        if self.args.list_new:
+            sys.exit()
+
+        # Prompt user to continue
+        if self.args.quiet:
+            approved = True
+        else:
+            approved = self.yes_or_no('Add them to your Slack team?')
+
+        # Check for approval
+        if not approved:
+            sys.exit()
+
+        # Add parrots
+        return self.add_parrots(to_add)
+
+    def report(self, added):
+        """Print added parrots and send Slack notification.
+
+        Args:
+            added (list): List of added Parrot emoji data
+
+        """
+        print(
+            'Successfully added {count} new parrots!'.format(count=len(added)),
+            file=sys.stdout
+        )
+
+        # Check if we need to alert a Slack channel
+        if self.args.channel is None:
+            return
+
+        # Set up message content
+        message = '*Added {count} new Party Parrot{s}!*'.format(
+            count=len(added),
+            s='s' if len(added) > 1 else ''
+        )
+
+        # Add each parrot to message
+        for new in added:
+            message += '\n+ :{slug}: `:{slug}:`'.format(**new)
+
+        # Send Slack message
+        sent = self.session.post(
+            self.__class__.API_ROOT.format('chat.postMessage'),
+            data={
+                'token': self.api_token,
+                'channel': self.args.channel,
+                'text': message
+            }
+        )
+        sent.raise_for_status()
+
+        # Get JSON response
+        sent_json = sent.json()
+
+        # Check for bad response from Slack
+        if not sent_json['ok']:
+            print(
+                'ERROR: Unable to send Slack message: {error}'.format(
+                    error=sent_json['error']
+                ),
+                file=sys.stderr
+            )
+
 
 def main():
     """Primary parroter function."""
@@ -699,10 +868,8 @@ def main():
 
     # Finish parroting
     if added:
-        print(
-            'Successfully added {count} new parrots!'.format(count=len(added)),
-            file=sys.stdout
-        )
+        parroter.report(added)
+
 
 # Run the parroter via CLI
 if __name__ == '__main__':
